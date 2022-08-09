834c77391a55fc0cfaa4588a80ed786b412d8959
abhiparate <abhiparate2523@gmail.com>
2022-08-09 21:11:36 +0530

    practical 1 part one completed

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,9 +1,16 @@
 fun main()
 {
-    var x:Int = 100
-    val y:Int = 200
-    val z = 300.95f
+    var i:Int = 22
+    var f:Float = 1.5f
+    var c:Char = 'T'
+    var s:String = "HMS"
+    var b:Boolean = false
+    var d:Double = 96.36
+    var l:Long = 338927847
+    var sh:Short = -2
+    var by:Byte = 127
 
-    println("x=$x\ny=$y\nz=$z")
+    println("Integer Value: $i\nFloat Value: $f\nCharacter Value: $c\nString Value: $s\nBoolean Value: $b")
+    println("Double Value: $d\nLong Value: $l\nShort Value: $sh\nBute Value: $by")
 
 }

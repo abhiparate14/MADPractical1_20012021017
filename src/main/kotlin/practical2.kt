4520552f72cabf18ff09fb866aefe4559d971ce9
abhiparate <abhiparate2523@gmail.com>
2022-08-09 21:28:50 +0530

    practical 1 part two completed review once

--- src/main/kotlin/practical2.kt
+++ src/main/kotlin/practical2.kt
@@ -1,4 +1,15 @@
 fun main()
 {
-
+    var i:Int = 10
+    println("Integer Value: $i")
+    var d:Double = i.toDouble()
+    println("Double Value (From Integer): $d")
+    var s:String = "10"
+    println("String Value: $s")
+    var value1:Int = s.toInt()
+    println("Integer Value1 (From String): $value1")
+    var value2:Int = s.toInt()
+    println("Integer Value2 (From String): $value2")
+    var d2:Double = s.toDouble()
+    println("Double Value (From String): $d2")
 }

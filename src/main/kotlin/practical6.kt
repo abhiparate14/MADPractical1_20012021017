fb36f4838da92df8287bf652febdf867806a9dc3
Student <dharmik.jmp@gmail.com>
2022-08-10 14:26:19 +0530

    question 6 update

--- src/main/kotlin/practical6.kt
+++ src/main/kotlin/practical6.kt
@@ -4,21 +4,31 @@ fun main()
     sub(111, 2222, -222)
     mul(111, 2222, -222)
     division(2222, 111)
+    mod(2222, 111)
 }
+// addition function
 fun add(v1:Int, v2:Int, v3: Int)
 {
     println("Addition of $v1, $v2, $v3 is ${v1 + v2 + v3}")
 }
+// subtraction function
 fun sub(v1:Int, v2:Int, v3:Int)
 {
    println("Subtraction of $v1, $v2, $v3 is ${v1 - v2 - v3}")
 }
+// multiplication function
 fun mul(v1:Int, v2:Int, v3:Int)
 {
     println("Multiplication of $v1, $v2, $v3 is ${v1 * v2 * v3}")
 }
+// division function
 fun division(v1:Int, v2:Int)
 {
     println("Divition of $v1, $v2 is ${v1 / v2}")
+}
+// modulo function
+fun mod(v1:Int, v2:Int)
+{
+    println("Modulo of $v1, $v2 is ${v1 % v2}")
 }
 
